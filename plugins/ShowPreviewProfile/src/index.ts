4f9b33f6dc9143c8a84de838664cbe5954a7510d
cywr <110328186+cywrx@users.noreply.github.com>
2026-01-10 18:40:33 +0300

    Update index.ts

--- plugins/ShowPreviewProfile/src/index.ts
+++ plugins/ShowPreviewProfile/src/index.ts
@@ -1,25 +1,40 @@
 import { findByProps, findByName } from "@vendetta/metro";
 import { after } from "@vendetta/patcher";
-import { NavigationNative } from "@vendetta/metro/common";
-import { showToast } from "@vendetta/ui/toasts";
+import { React, ReactNative as RN, NavigationNative } from "@vendetta/metro/common";
 
 const UserStore = findByProps("getCurrentUser");
-const UserProfileRenderer = findByName("UserProfile", false);
+const UserProfileRenderer = findByName("UserProfileEditor", false) || findByName("UserProfileSettings", false);
+const Button = findByProps("Button").Button;
 
 let unpatch: () => void;
 
 export default {
   onLoad() {
-    unpatch = after("default", UserProfileRenderer, (args, res) => {
-      
+    unpatch = after("default", UserProfileRenderer, (_, res) => {
       const currentUser = UserStore.getCurrentUser();
+      const navigation = NavigationNative.useNavigation();
 
       if (!res?.props?.children) return;
-      
+
+      const previewButton = React.createElement(Button, {
+        text: "Preview Profile",
+        size: "small",
+        color: "brand",
+        onPress: () => {
+          navigation.push("UserProfile", {
+            userId: currentUser.id,
+          });
+        },
+        style: { marginVertical: 10 }
+      });
+
+      const list = res.props.children.props?.children || res.props.children;
+      if (Array.isArray(list)) {
+        list.unshift(previewButton);
+      }
     });
   },
-
   onUnload() {
-    if (unpatch) unpatch();
-  },
+    unpatch?.();
+  }
 };
